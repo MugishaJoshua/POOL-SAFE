fd4ec51d67909f1c212120b24ddb5df636cd0fb6
Joshua Mugisha <mugishajoshua567@gmail.com>
2026-06-07 18:51:16 +0200

    New Database running

--- core/settings.py
+++ core/settings.py
@@ -53,7 +53,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR}/db.sqlite3"
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
