df68b48850b99f6a9d4cb2f173050005356cd4e5
Joshua Mugisha <mugishajoshua567@gmail.com>
2026-06-25 17:28:45 +0200

    New bug fix

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env file
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
